ad37f0779186473797914331e517e0d0ff0299c2
Tetiana-Nazarenko <nnn27725@gmail.com>
2023-08-27 21:43:00 +0300

    changes imageGalleryItem-hooks

--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -1,37 +1,33 @@
-import { Component } from 'react';
+import { useState } from 'react';
+
 import { Item, Img } from './ImageGalleryItem.styled';
 import { ModalImage } from 'components/Modal/Modal';
 
-// Классовый компонент ImageItem
-export class ImageItem extends Component {
-  state = {
-    isOpen: false, // Хранит состояние модального окна (открыто или закрыто)
+export const ImageItem = ({ image }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const openModal = () => {
+    setIsOpen(true);
   };
 
-  openModal = () => {
-    this.setState({ isOpen: true });
+  const closeModal = () => {
+    setIsOpen(false);
   };
-  closeModal = () => {
-    this.setState({ isOpen: false });
-  };
-  render() {
-    const { image } = this.props;
 
-    return (
-      <>
-        <Item>
-          <Img
-            src={image.webformatURL} // URL маленького изображения
-            alt={image.tags} // Теги изображения
-            onClick={this.openModal} // Обработчик клика для открытия модального окна
-          />
-          {/* {showModal && ( // Если showModal равно true, отображаем модальное окно */}
-          <ModalImage onOpen={this.state.isOpen} onClose={this.closeModal}>
-            <img src={image.largeImageURL} alt={image.tags} />
-          </ModalImage>
-          {/* )} */}
-        </Item>
-      </>
-    );
-  }
-}
+  return (
+    <>
+      <Item>
+        <Img
+          src={image.webformatURL} // URL маленького изображения
+          alt={image.tags} // Теги изображения
+          onClick={openModal} // Обработчик клика для открытия модального окна
+        />
+        {/* {showModal && ( // Если showModal равно true, отображаем модальное окно */}
+        <ModalImage onOpen={isOpen} onClose={closeModal}>
+          <img src={image.largeImageURL} alt={image.tags} />
+        </ModalImage>
+        {/* )} */}
+      </Item>
+    </>
+  );
+};
